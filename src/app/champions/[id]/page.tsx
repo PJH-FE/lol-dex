3ae057326c42b7f9f37387c9548040657e8b922d
PJH-FE <sjrmd789@gmail.com>
2024-10-08 02:41:33 +0900

    update: remove swiper

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -115,12 +115,6 @@ const Champions = async ({ params }: { params: { id: string } }) => {
                 );
               })}
             </ul>
-
-            <div className="skins mt-6">
-              <h3 className="font-bold text-xl mb-4">Skins</h3>
-
-              <SkinSwiper championInfo={championInfo} />
-            </div>
           </div>
         </div>
       </div>
