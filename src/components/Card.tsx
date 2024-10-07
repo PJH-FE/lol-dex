30423070a6f2300096b6af642f369ea9f9406a48
PJH-FE <sjrmd789@gmail.com>
2024-10-08 02:33:53 +0900

    update: Link태그 수정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,10 @@ type ChampionProps = {
 
 const Card: React.FC<ChampionProps> = ({ champion }) => {
   return (
-    <Link href={`/champions/${champion.id}`} className="w-[calc(25%-18px)] md:w-[calc((100%/7)-(144px/7))]">
+    <Link
+      href="/champions/[id]"
+      as={`/champions/${champion.id}`}
+      className="w-[calc(25%-18px)] md:w-[calc((100%/7)-(144px/7))]">
       <Image
         src={`https://ddragon.leagueoflegends.com/cdn/${champion.version}/img/champion/${champion.image.full}`}
         alt={champion.name}
